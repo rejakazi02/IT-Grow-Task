c9724abdf47b885de594642ecdfb44c77818bf06
Reja Kazi <rejakazi02@gmail.com>
2023-06-07 00:45:50 +0600

    Add movies

--- src/app/pages/home/home-routing.module.ts
+++ src/app/pages/home/home-routing.module.ts
@@ -1,11 +1,16 @@
 import { HomeComponent } from './home.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MovieDetailsComponent } from './movie-details/movie-details.component';
 
 const routes: Routes = [
   {
     path:"",
     component:HomeComponent
+  },
+  {
+    path:"movie/:id",
+    component:MovieDetailsComponent
   }
 ];
 
--- src/app/pages/home/home.module.ts
+++ src/app/pages/home/home.module.ts
@@ -1,19 +1,13 @@
-
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
-
+import { MovieDetailsComponent } from './movie-details/movie-details.component';
+import { MatDialogModule } from '@angular/material/dialog';
 
 @NgModule({
-  declarations: [
-    HomeComponent
-  ],
-    imports: [
-        CommonModule,
-        HomeRoutingModule,
-
-    ]
+  declarations: [HomeComponent, MovieDetailsComponent],
+  imports: [CommonModule, HomeRoutingModule, MatDialogModule],
 })
-export class HomeModule { }
+export class HomeModule {}
